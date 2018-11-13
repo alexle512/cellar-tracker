4b6757d854c21770786319432c018e673c782451
ttlekich <ttlekich@gmail.com>
2018-11-13 14:26:44 -0600

    added paths to app

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+/**
+ * Imports
+ */
 const express = require("express")
-const app = express()
+const path = require("path")
+const bodyParser = require("body-parser")
 const mustacheExpress = require("mustache-express")
 const models = require("./models")
-const bodyParser = require("body-parser")
-const port = 3000
 
+const search = require("./routes/search")
+
+/**
+ * Init
+ */
+const app = express()
 app.engine("mustache", mustacheExpress())
-app.set("views", "./views")
+
+// View Engine
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "mustache")
 
+// body-parser
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Public Files
+app.use(express.static(path.join(__dirname, "public")))
+
+/**
+ * Custon Middleware
+ */
+
+/**
+ * Route Config
+ */
+
+// SEARCH
+app.use("/search", search)
+
+// CELLAR
 
 let cellar = models.wishlist.build({
   beverage: "Stolichnaya Elit",
   price: 25.0,
   notes: "Graduation Party"
 })
-//   cellar.save().then(function(){
-// })
 
 app.get("/cellar", function(req, res) {
   models.cellar.findAll().then(function(cellers) {
@@ -40,14 +66,42 @@ app.post("/delete-wishlist", function(req, res) {
 
     res.render("delete-wishlist", { beverage: wishlist.beverage })
   })
-
-  // wishlist = wishlist.filter(function(list) {
-  //   return list.beverage != beverage
 })
 
 app.get("/wishlist", function(req, res) {
   models.wishlist.findAll().then(function(wishlists) {
     res.render("wishlist", { wishlists: wishlists })
+  })
+})
+
+// Reviews
+const review = models.review.build({
+  rating: 4,
+  product: "Johnnie Walker",
+  category: "Whiskey"
+})
+
+app.get("/reviews", function(req, res) {
+  models.review.findAll().then(function(reviews) {
+    res.render("index", { reviews: reviews })
+  })
+})
+
+app.post("/reviews", function(req, res) {
+  let product = req.body.product
+  let rating = req.body.rating
+  let category = req.body.category
+  const review = models.review.build({
+    rating: rating,
+    product: product,
+    category: category
+  })
+
+  // saving review
+  review.save().then(function(newReview) {
+    models.review.findAll().then(function(reviews) {
+      res.render("index", { reviews: reviews })
+    })
   })
 })
 
