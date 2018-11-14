4b4c2accead4b2a127ba2c2e2be2228626591dd0
ttlekich <ttlekich@gmail.com>
2018-11-14 15:38:12 -0600

    adding in filters for different types of spirits

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -22,11 +22,11 @@ let allBeers = []
 /**
  * Fetch From Database on Server Load
  */
-const onLoad = async () => {
-  allWines = await wineDB.findAll()
-  allBeers = await beerDB.findAll()
-}
-onLoad()
+// const onLoad = async () => {
+//   allWines = await wineDB.findAll()
+//   allBeers = await beerDB.findAll()
+// }
+// onLoad()
 // End Fetch From db on server load
 
 const displaySearch = async (req, res) => {
@@ -34,18 +34,43 @@ const displaySearch = async (req, res) => {
 }
 
 const querySearch = async (req, res) => {
+  const type = req.params.type
   const itemName = req.body.itemName
+  const limit = 20
+  let offset = 0
+  switch (type) {
+    case "wine":
+    case "beer":
+    case "spirits":
+    default:
+  }
+
   /**
    * Fetch From Database / Search
    */
-  // const beers =
-  //   (await beerDB.findAll({
-  //     where: { beer_name: { $ilike: `%${itemName}%` } }
-  //   })) || []
-  // const wines =
-  //   (await wineDB.findAll({
-  //     where: { title: { $ilike: `%${itemName}%` } }
-  //   })) || []
+  const beers = (await beerDB.findAndCountAll({
+    where: {
+      beer_name: { $ilike: `%${itemName}%` }
+    },
+    limit: limit,
+    offset: offset
+  })) || [(rows = [])]
+  const wines = (await wineDB.findAndCountAll({
+    where: { title: { $ilike: `%${itemName}%` } },
+    limit: limit,
+    offset: offset
+  })) || [(rows = [])]
+  searchResults = { beers: beers.rows, wines: wines.rows }
+  res.redirect("/search")
+}
+
+const searchBeers = async (req, res) => {}
+
+const searchWines = async (req, res) => {}
+
+const searchSpirits = async (req, res) => {}
+
+const fetchFromInit = () => {
   /**
    * Fetch From Database on Server Load
    */
@@ -59,11 +84,38 @@ const querySearch = async (req, res) => {
       beer.beerName.toUpperCase().includes(itemName.toUpperCase()) ||
       beer.breweryName.toUpperCase().includes(itemName.toUpperCase())
   )
-  searchResults = { beers, wines }
-  res.redirect("/search")
 }
 
 module.exports = {
   displaySearch,
   querySearch
 }
+
+/**
+router.get("/:page", (req, res) => {
+  let limit = 50 // number of records per page
+  let offset = 0
+  db.user
+    .findAndCountAll()
+    .then(data => {
+      let page = req.params.page // page number
+      let pages = Math.ceil(data.count / limit)
+      offset = limit * (page - 1)
+      db.user
+        .findAll({
+          attributes: ["id", "first_name", "last_name", "date_of_birth"],
+          limit: limit,
+          offset: offset,
+          $sort: { id: 1 }
+        })
+        .then(users => {
+          res
+            .status(200)
+            .json({ result: users, count: data.count, pages: pages })
+        })
+    })
+    .catch(function(error) {
+      res.status(500).send("Internal Server Error")
+    })
+})
+*/
