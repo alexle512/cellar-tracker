b79b2dfd76b47d1601efcf3c71f138f3688e0efd
ttlekich <ttlekich@gmail.com>
2018-11-16 14:49:46 -0600

    help

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ const path = require("path")
 const Sequelize = require("sequelize")
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || "development"
-console.log(env)
+console.log(`env: ${env}`)
 const config = require(__dirname + "/../config/config.js")[env]
-console.log(config)
+console.log(`config: ${config}`)
 const db = {}
 
 let sequelize
